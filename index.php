d2dbcb96673b08341f5af66581c4f73e8e41b48c
AbdoulayeGueyeMbaye <mbayeabdoulayegueye@gmail.com>
2026-07-02 03:14:00 +0000

    initialier les tableaux

--- index.php
+++ index.php
@@ -9,6 +9,9 @@ require_once __DIR__ . '/model/product.model.php';
 require_once __DIR__ . '/service/service.php';
 require_once __DIR__ . '/view/client.view.php';
 require_once __DIR__ . '/view/product.view.php';
+require_once __DIR__ . '/controller/product.controller.php';
+require_once __DIR__ . '/controller/commande.controller.php';
+
 
 
 
