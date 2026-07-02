115350f25a4bf84cbb546f6aacb7fb21b5919025
AbdoulayeGueyeMbaye <mbayeabdoulayegueye@gmail.com>
2026-07-02 03:03:46 +0000

    creation des fichiers en mvc/initialiser les tableaux

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+
+<?php
+
+require_once __DIR__ . '/model/client.model.php';
+require_once __DIR__ . '/model/commande.model.php';
+require_once __DIR__ . '/model/product.model.php';
+require_once __DIR__ . '/service/service.php';
+require_once __DIR__ . '/view/client.view.php';
+require_once __DIR__ . '/view/product.view.php';
+
+
+
+
